43a5e823bba83ef461725a547a7828159d8cc739
K555L <123kingkc@gmail.com>
2021-05-19 18:41:48 +0530

    Initial commit

---
+++ app/src/main/java/com/example/instagramclone/CreatePostActivity.kt
@@ -0,0 +1,30 @@
+package com.example.instagramclone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.instagramclone.daos.PostDao
+import kotlinx.android.synthetic.main.activity_create_post.*
+
+class CreatePostActivity : AppCompatActivity() {
+
+    private lateinit var postDao: PostDao
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_post)
+
+        postDao = PostDao()
+
+        postButton.setOnClickListener {
+            val input=postInput.text.toString()
+            if(input.isNotEmpty()) {
+                postDao.addPost(input)
+                finish()
+            }
+        }
+        setUpRecyclerView()
+    }
+    private fun setUpRecyclerView() {
+
+    }
+}
---
+++ app/src/main/java/com/example/instagramclone/models/User.kt
@@ -0,0 +1,5 @@
+package com.example.instagramclone.models
+
+data class User(val uid: String = "",
+                val displayName: String? = "",
+                val imageUrl: String = "")
